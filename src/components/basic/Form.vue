d57f44afba63fd905d08fa3785c607ad64b5e813
Loki <blakedonovan@starbase.com>
2022-05-01 12:27:37 +0200

    first commit

---
+++ src/components/basic/Form.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="card">
+        <div class="card_title">
+            {{ compTitle }}
+        </div>
+        <form @submit.prevent>
+
+            <label for="loginName">Benutzer</label>
+            <input id="loginName" type="text" v-model="loginName">
+
+            <label for="passwort">Passwort</label>
+            <input id="passwort" type="password" v-model="password">
+
+            <button @click="showName">Name</button>
+        </form>
+    </div>
+    {{ loginName }} {{ password }}
+</template>
+
+
+<script setup>
+import { onMounted, ref, computed } from 'vue'
+// anything declared in script setup is directly useable in the template
+// macro function to define Props
+defineProps({
+
+    userShort: {
+        type: String,
+        default: ""
+    }
+
+});
+
+// declare variable and make it reactive with ref
+const compTitle = ref("Form")
+// computed variable , this one is read-only!
+let number = computed(() => number = 2)
+
+// v-model var , made reactive
+let loginName = ref("")
+let password = ref("")
+// define functions 
+
+function showUP() {
+    console.log(number)
+}
+
+
+function showName() {
+    alert(loginName)
+}
+// call function on lifecycle 
+onMounted(
+    () => showUP()
+)
+
+</script>
+
+<style>
+form {
+
+    border: 0.5em solid black;
+}
+
+input[type=text] {
+
+    border-radius: 5px;
+    padding: 5px;
+}
+
+.card_title {
+
+    background: blue;
+    max-width: 250px;
+    border-radius: 5px;
+}
+</style>
---
+++ src/views/BookComparison.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import {onMounted} from 'vue'
+
+let booked :string[];
+let bookAttempt: string[];
+let cantBook:string[]; 
+// saved bookings by date and place id
+booked = ['2022-01-07','2022-01-08'];
+
+// user entered dates for specific place 
+bookAttempt=['2022-01-08','2022-01-11']
+
+
+function bookingCompare(bookedDates:string[],bookAttemptDates:string[]){
+
+
+const cantBook = bookedDates
+.filter(value => 
+bookAttemptDates.includes(value))
+.filter((value, index, self) =>
+ self.indexOf(value) === index);
+
+
+
+return cantBook;
+}
+
+cantBook = bookingCompare(booked,bookAttempt);
+
+//onMounted(()=>bookingCompare(booked,bookAttempt))
+
+
+</script>
+
+<template>
+<div>
+<h1>Booking Function</h1>
+
+
+</div>
+<h2>already booked</h2>
+<div v-for="book in booked" >
+{{book}}
+</div>
+<h2>bookAttempt by user</h2>
+<div v-for="attempt in bookAttempt" >
+{{attempt}}
+</div>
+<hr>
+<h2>cant be booked by user</h2>
+<div v-for="cantBeBooked in cantBook">
+{{cantBeBooked}}
+</div>
+</template>
