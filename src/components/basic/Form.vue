8d94de48d737efb42887842834fa2d8e343d9ac5
Loki <blakedonovan@starbase.com>
2022-05-05 22:39:16 +0200

    h

--- src/components/basic/Form.vue
+++ src/components/basic/Form.vue
@@ -1,77 +1,104 @@
 <template>
-    <div class="card">
-        <div class="card_title">
-            {{ compTitle }}
-        </div>
-        <form @submit.prevent>
 
+
+    <form @submit.prevent :class="[formStyle]">
+        <div class="col-25">
             <label for="loginName">Benutzer</label>
-            <input id="loginName" type="text" v-model="loginName">
+
+        </div>
+ <div class="col-75">
+        <input id="loginName" type="text" v-model="loginName">
+        </div>
+        <p></p>
+        <div class="col-25">
 
             <label for="passwort">Passwort</label>
-            <input id="passwort" type="password" v-model="password">
 
-            <button @click="showName">Name</button>
-        </form>
-    </div>
-    {{ loginName }} {{ password }}
+        </div>
+         <div class="col-75">
+        <input id="passwort" type="password" v-model="password">
+</div>
+        <button>fire</button>
+    </form>
+
 </template>
 
 
-<script setup>
+<script setup lang="ts">
 import { onMounted, ref, computed } from 'vue'
 // anything declared in script setup is directly useable in the template
 // macro function to define Props
 defineProps({
 
+
+    formStyle: {
+        type: String,
+        default: "normal"
+    },
+
     userShort: {
         type: String,
         default: ""
-    }
+    },
+
+
 
 });
 
 // declare variable and make it reactive with ref
 const compTitle = ref("Form")
 // computed variable , this one is read-only!
-let number = computed(() => number = 2)
+
 
 // v-model var , made reactive
 let loginName = ref("")
 let password = ref("")
-// define functions 
-
-function showUP() {
-    console.log(number)
-}
+// define functions
 
 
-function showName() {
-    alert(loginName)
-}
+
+
+
 // call function on lifecycle 
-onMounted(
-    () => showUP()
-)
 
 </script>
 
-<style>
+<style lang="scss" scoped>
+* {
+    box-sizing: border-box;
+}
+html{
+
+    background-color: antiquewhite;
+}
 form {
 
-    border: 0.5em solid black;
+    background: $form-input-bgColor;
+    border: 0.5em solid rgb(0, 255, 115);
+    border-radius: $form-border-radius;
+    align-items: center;
+    padding: 20px;
+    min-width: 600px;
 }
 
-input[type=text] {
 
-    border-radius: 5px;
-    padding: 5px;
+input[type] {
+    border-radius: $input-type-border-radius;
+    padding: $input-type-padding;
+    color: rgb(255, 123, 0);
+    resize: vertical;
+
 }
 
-.card_title {
+.col-25 {
+    float: left;
+    width: 25%;
+    margin-top: 6px;
+}
 
-    background: blue;
-    max-width: 250px;
-    border-radius: 5px;
+.col-75 {
+    float: left;
+    width: 75%;
+    margin-top: 6px;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,22 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   server: {
     origin: 'http://localhost:3000/frontend/',
     host: '0.0.0.0',
     fs: {
         strict: true,
     }
+},
+css: {
+  preprocessorOptions: {
+    scss: { 
+       // example : additionalData: `@import "./src/design/styles/variables";`
+       // dont need include file extend .scss
+       additionalData: '@import "@/styles/index.scss";',
+   },
+  },
 },
   plugins: [vue(), vueJsx()],
   resolve: {
