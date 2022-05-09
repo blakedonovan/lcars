20d0c7ac75054935b4a82de7dd122437f8f38084
Tobias Voegele <atlan084@outlook.de>
2022-05-09 13:26:42 +0200

    fg

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="wrapper">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/test">Test</RouterLink>
-      </nav>
-    </div>
-  </header>
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">Form</RouterLink>
+      <RouterLink to="/calendar">Calendar</RouterLink>
+    </nav>
+  </div>
+
 
   <RouterView />
 
@@ -25,14 +23,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 <style>
 @import "@/assets/base.css";
 
-#app {
-  max-width: 1280px;
-  min-height: auto;
-  margin: 0 auto;
-  padding: 2rem;
 
-  font-weight: normal;
-}
 
 header {
   line-height: 1.5;
